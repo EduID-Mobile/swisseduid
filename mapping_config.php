822406f6b83ff1c1d8cae308894370664b24cb47
Christian Glahn <cpglahn@gmail.com>
2017-01-19 11:23:33 +0100

    mapping configuration stub.

---
+++ mapping_config.php
@@ -0,0 +1,14 @@
+<input type="hidden" id="azp" name="azp" value="<?php echo $azpInfo->id; ?>" />
+<div id="settings">
+    <div>
+        <ul>
+            <!-- process the mapping array -->
+            <li>
+            </li>
+        </ul>
+        <div>
+            <input type="submit" id="storemap" name="storemap" value="Change Mapping">
+            <input type="submit" id="cancel" name="storemap" value="Cancel">
+        </div>
+    </div>
+</div>
