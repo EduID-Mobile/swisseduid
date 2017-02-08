9468a903e24f21bcbbaf7a3d648caa66eca9ce09
Christian Glahn <cpglahn@gmail.com>
2017-02-08 16:52:17 +0100

    fixes #14

--- rsd.php
+++ rsd.php
@@ -1,7 +1,7 @@
 <?php
     // note: OAuth has several endpoints
     $apis['org.ietf.oauth2'] = [
-        "apiLink" => "auth/eduid/eduid.php",
+        "apiLink" => "auth/oauth2/cb.php",
         "transport" => array("REST")
     ];
 ?>
