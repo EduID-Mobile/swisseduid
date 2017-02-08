d2436c99e59e3d3631a08dc5ac030c43096e5e55
Luca Bösch <luca.boesch@bfh.ch>
2017-02-08 15:10:36 +0100

    Localized strings.
    Signed-off-by: Luca Bösch <luca.boesch@bfh.ch>

--- lang/en/auth_oauth2.php
+++ lang/en/auth_oauth2.php
@@ -4,6 +4,12 @@ $string['pluginname'] = 'Swiss edu-ID authentication plugin';
 $string['eduid_user_info_endpoint'] = 'Swiss edu-ID user information endpoint';
 $string['service_token_duration'] = 'Service token duration in hours';
 $string['app_token_duration'] = 'App token duration in hours';
+$string['generate_private_key'] = 'Generate Private Key';
+$string['authority_name'] = 'Authority name';
+$string['oauth2_redirect_uri_is'] = 'Your OAuth2 redirect_uri is';
+$string['public_key_is'] = 'Your Public Key is:';
+$string['registered_auth_services'] = 'Registered Authorization Services';
+$string['private_key_present'] = 'Private Key is present';
 // authority table
 $string['eduid_add_new_authority'] = 'Add New Authority';
 $string['eduid_drop_authority_entry'] = 'Drop Authority';
--- views/config.php
+++ views/config.php
@@ -4,19 +4,19 @@
     <div id="private_key">
 <?php
 if (isset($PK)) {
-    echo "<div>Private Key is present<div>";
+    echo "<div>" . get_string('private_key_present', 'auth_oauth2') . "</div>";
 }
 ?>
     <div>
         <textarea id="pk" name="pk"></textarea>
     </div>
     <div>
-        <input type="submit" id="gen_key" name="gen_key" value="Generate Private Key"/>
+        <input type="submit" id="gen_key" name="gen_key" value="<?php echo get_string('generate_private_key', 'auth_oauth2'); ?>"/>
     </div>
     </div>
     <!-- list of authorities -->
     <div id="authorityList">
-        <h2>Registered Authorization Services</h2>
+        <h2><?php echo get_string('registered_auth_services', 'auth_oauth2'); ?></h2>
         <ul>
             <?php
 foreach ($authorities as $azp) {
@@ -26,13 +26,13 @@ foreach ($authorities as $azp) {
         </ul>
         <div>
                 <p>
-                    Your OAuth2 redirect_uri is <pre><?php echo "$tlaurl/cb"?></pre>
+                    <?php echo get_string('oauth2_redirect_uri_is', 'auth_oauth2'); ?> <pre><?php echo "$tlaurl/cb"?></pre>
                 </p>
                 <p>
-                    Your Public Key is:
+                    <?php echo get_string('public_key_is', 'auth_oauth2'); ?>
                 </p>
                 <p><?php echo $pubKey; ?></p>
-                <p><input id="name" name="name" type="text" placeholder="Authority Name"></p>
+                <p><input id="name" name="name" type="text" placeholder="<?php echo get_string('authority_name', 'auth_oauth2'); ?>"></p>
                 <p><input id="client_id" name="client_id" type="text" placeholder="client_id (as provided by the authority)"></p>
                 <p><input id="url" name="url" type="text" placeholder="authory OAuth2 Url"></p>
                 <input type="hidden" id="flow" name="flow" value="code"/>
